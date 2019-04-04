427e4f8c4be69e422e30915381a606f653c18e93
ovshake <abhishek16005@iiitd.ac.in>
2019-04-04 22:13:09 +0530

    init.py debugged

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,7 @@
-
+import networkx as nx 
+import os
+from os import listdir
+import pickle
 #global Variables
 INDEX_OFFSET = 6
 CITE_OFFSET = 3
@@ -8,82 +11,83 @@ EMPTY = ""
 
 
 def get_pickle_dump(filepath,filename):
-"""
-@Params:
-filename: filename inside the ./dumps folder
-@Returns:
-Variable: Returns the pickled variable
-"""
+	"""
+	@Params:
+	filename: filename inside the ./dumps folder
+	@Returns:
+	Variable: Returns the pickled variable
+	"""
+
 	with open(filepath +'/'+filename+'.pickle','rb') as handle:
 		Variable = pickle.load(handle)
 	return Variable
 
 
 def dump_file(filepath,filename, Variable):
-"""
-@Params:
-filename: filename inside the ./dumps folder for dumping
-Variable: Variable to dump inside the file
-@Returns:
-None
-"""
+	"""
+	@Params:
+	filename: filename inside the ./dumps folder for dumping
+	Variable: Variable to dump inside the file
+	@Returns:
+	None
+	"""
 	with open(filepath +'/'+filename+'.pickle','wb') as handle:
 		pickle.dump(Variable,handle,protocol=pickle.HIGHEST_PROTOCOL)
 
 def extract_paper_id(line):
-"""
-@Params:
-line: text line
-@Returns: 
-paper_id: extracts the paper-id enclosed within "[*]"
-"""
+	"""
+	@Params:
+	line: text line
+	@Returns: 
+	paper_id: extracts the paper-id enclosed within "[*]"
+	"""
 	line = line.split("[")
 	# "-2" is to remove the last 2 characters for CLRF line endings.
-	paper_id = line_array[-1][:-2]
+	paper_id = line[-1][:-2]
 	return paper_id
 
 
 def parse_MAS_dataset(path):
-"""
-@Params
-path: Path to the MAS Dataset
-@Returns
-A global citation graph with edges from the citer to the cited.
-"""
+	"""
+	@Params
+	path: Path to the MAS Dataset
+	@Returns
+	A global citation graph with edges from the citer to the cited.
+	"""
 
 	global_citation_graph = nx.DiGraph() 
 	fields = os.listdir(path)
 	for field in fields:
+		print(field)
 		if field.split("_")[-1] != 'data.txt': #if not a data text file
 			continue
 		dataset_path = path + "/" + field
-		file = open(dataset_path, 'r')
-		line = file.readline()
-		line = (line.encode('ascii', 'ignore')).decode("utf-8")
-		index_name = EMPTY
+		file = open(dataset_path, 'rb')
+		line = file.readline().decode('utf8')
+		name = EMPTY
 		while line:
 			if line[:INDEX_OFFSET] == '#index':
-				paper_name = line[INDEX_OFFSET:-1]
+				name = line[INDEX_OFFSET:-1]
 
 			elif line[:CITE_OFFSET] == '#%*' and name != EMPTY:
 				cited_by_index_name = extract_paper_id(line) 
-				global_citation_graph.add_edge(index_name , cited_by_index_name)
+				global_citation_graph.add_edge(name , cited_by_index_name)
 
 			elif line[:CITE_OFFSET] == "#$*" and name != EMPTY:
 				citing_index_name = extract_paper_id(line) 
-				global_citation_graph.add_edge(citing_index_name , index_name)
-			line = file.readline()
-			line = (line.encode('ascii', 'ignore')).decode("utf-8")
+				global_citation_graph.add_edge(citing_index_name , name)
+			line = file.readline().decode('utf8')
 
-		return global_citation_graph
+
+	return global_citation_graph
 
 def parse_dates(path):
-"""
-@Params:
-path: Path to MAS Dataset
-@Returns: 
-PAPER_YEAR_DICT: Dictionary with paper ids as keys publishing years as venues
-"""
+	"""
+	@Params:
+	path: Path to MAS Dataset
+	@Returns: 
+	PAPER_YEAR_DICT: Dictionary with paper ids as keys publishing years as venues
+	"""
 	PAPER_YEAR_DICT = {}
 	fields = listdir(path)
 	for field in fields:
@@ -91,9 +95,9 @@ PAPER_YEAR_DICT: Dictionary with paper ids as keys publishing years as venues
 			continue
 
 		dataset_path = path + "/" + field
-		file = open(dataset_path, 'r')
-		line = file.readline()
-		line = (line.encode('ascii', 'ignore')).decode("utf-8")
+		file = open(dataset_path, 'rb')
+		line = file.readline().decode('utf8')
+		# line = (line.encode('ascii', 'ignore')).decode("utf-8")
 		paper_name = EMPTY
 
 		while line:
@@ -121,24 +125,24 @@ PAPER_YEAR_DICT: Dictionary with paper ids as keys publishing years as venues
 				PAPER_YEAR_DICT[paper_name] = year
 				paper_name = EMPTY
 
-			line = file.readline()
-			line = (line.encode('ascii', 'ignore')).decode("utf-8")
+			line = file.readline().decode('utf8')
+			# line = (line.encode('ascii', 'ignore')).decode("utf-8")
 	
 	return PAPER_YEAR_DICT
 
 
 
 def check_edge_validity(global_citation_graph, PAPER_YEAR_DICT):
-"""
-@Params:
-global_citation_graph: The global citation graph with all edges
-PAPER_YEAR_DICT: Contains the publishing of the papers
+	"""
+	@Params:
+	global_citation_graph: The global citation graph with all edges
+	PAPER_YEAR_DICT: Contains the publishing of the papers
 
-@Returns:
-global_citation_graph: The global citation graph with noisy edges removed 
-						i.e. those edges whose citer's publishing year is 
-						less than cited's publishing year
-"""
+	@Returns:
+	global_citation_graph: The global citation graph with noisy edges removed 
+							i.e. those edges whose citer's publishing year is 
+							less than cited's publishing year
+	"""
 	edge_set = list(global_citation_graph.edges())
 
 	for edge_ in edge_set:
@@ -161,15 +165,15 @@ global_citation_graph: The global citation graph with noisy edges removed
 
 
 def remove_cycles(global_citation_graph):
-"""
-@Params:
-global_citation_graph
+	"""
+	@Params:
+	global_citation_graph
 
-@Returns: 
-global_citation_graph: with no cycles due to noisy data by randomly removing one edge in the cycle
-						any subgraphs
+	@Returns: 
+	global_citation_graph: with no cycles due to noisy data by randomly removing one edge in the cycle
+							any subgraphs
 
-"""
+	"""
 	node_list = list(global_citation_graph.nodes())
 
 	for paper in node_list:
@@ -207,14 +211,14 @@ global_citation_graph: with no cycles due to noisy data by randomly removing one
 
 
 def IDT_init(global_citation_graph):
-"""
-@Params:
-global_citation_graph: The global citation graph 
+	"""
+	@Params:
+	global_citation_graph: The global citation graph 
 
-@Returns:
-IDT: a dictionary with key as the paper index and value as the IDT (Influence Dispersion Tree)
-IDT_root_to_leaf_paths: Dict with keys as Paper Name and value as All Root to leaf paths
-"""
+	@Returns:
+	IDT: a dictionary with key as the paper index and value as the IDT (Influence Dispersion Tree)
+	IDT_root_to_leaf_paths: Dict with keys as Paper Name and value as All Root to leaf paths
+	"""
 	IDT = {}
 	IDT_root_to_leaf_paths = {} 
 	nodes_with_cycle = 0
@@ -265,14 +269,14 @@ IDT_root_to_leaf_paths: Dict with keys as Paper Name and value as All Root to le
 
 
 def all_edges_in_visited(IDG,p, visited):
-"""
-@Params: 
-IDG: IDG of the paper 
-p: paper whose neighbours we need to check
-visited: set of nodes which are visited
-@Returns:
-True if all the neighbours are visited else False
-"""
+	"""
+	@Params: 
+	IDG: IDG of the paper 
+	p: paper whose neighbours we need to check
+	visited: set of nodes which are visited
+	@Returns:
+	True if all the neighbours are visited else False
+	"""
 	edge_list = list(IDG.out_edges(nbunch = [p]))
 	for e in edge_list:
 		if e[1] not in visited:
@@ -281,14 +285,14 @@ True if all the neighbours are visited else False
 
 
 def get_parent_with_most_depth(depth_dict , IDG, p):
-"""
-@Params: 
-IDG: IDG of the paper 
-p: paper whose neighbours we need to check
-depth_dict: Dict with all the depth of the papers in the IDG
-@Returns:
-The neighbour which has the most depth and has been visited.
-"""
+	"""
+	@Params: 
+	IDG: IDG of the paper 
+	p: paper whose neighbours we need to check
+	depth_dict: Dict with all the depth of the papers in the IDG
+	@Returns:
+	The neighbour which has the most depth and has been visited.
+	"""
 	edge_list = list(IDG.out_edges(nbunch = [p]))
 	max_depth = -1
 	ancestor_paper = EMPTY
@@ -299,14 +303,14 @@ The neighbour which has the most depth and has been visited.
 	return ancestor_paper
 
 def remove_edges_except(paper, ancestor_paper , IDG):
-"""
-@Params: 
-IDG: IDG of the paper 
-paper: paper whose neighbours we need to check
-ancestor_paper: The ancestor with whom we need to preserve the edges
-@Returns:
-The IDG with all but the given ancestor's edge removed
-"""
+	"""
+	@Params: 
+	IDG: IDG of the paper 
+	paper: paper whose neighbours we need to check
+	ancestor_paper: The ancestor with whom we need to preserve the edges
+	@Returns:
+	The IDG with all but the given ancestor's edge removed
+	"""
 	edge_list = list(G.out_edges(nbunch = paper))
 	for e in edge_list:
 		if e[1] != ancestor_paper:
@@ -342,32 +346,32 @@ def convert_IDG_to_IDT(IDG):
 	return IDT_, total_branches
 
 def get_max_idi(num_citations):
-"""
-@Params: 
-num_citations: Number of citations of a paper whose max IDI is to be calculated 
-@Returns:
-The max IDI of the paper
-"""
+	"""
+	@Params: 
+	num_citations: Number of citations of a paper whose max IDI is to be calculated 
+	@Returns:
+	The max IDI of the paper
+	"""
 	return num_citations
 
 def get_min_idi(num_citations):
-"""
-@Params: 
-num_citations: Number of citations of a paper whose min IDI is to be calculated 
-@Returns:
-The min IDI of the paper
-"""
+	"""
+	@Params: 
+	num_citations: Number of citations of a paper whose min IDI is to be calculated 
+	@Returns:
+	The min IDI of the paper
+	"""
 	return (num_citations - int((num_citations - 1) / 2))*(float(1 + int((num_citations - 1) / 2)))
 
 def get_idi_till_year(IDT, year, PAPER_YEAR_DICT):
-"""
-@Params: 
-IDT: IDT of the paper 
-year: Year untill which IDI is to be calculated. Nodes will be removed from the IDT accordingly.
-PAPER_YEAR_DICT: Dictionary with paper ids as keys and year of publication as values.
-@Returns:
-The IDI of the paper. Max and min IDI corresponding to the number of citations are also returned.
-"""
+	"""
+	@Params: 
+	IDT: IDT of the paper 
+	year: Year untill which IDI is to be calculated. Nodes will be removed from the IDT accordingly.
+	PAPER_YEAR_DICT: Dictionary with paper ids as keys and year of publication as values.
+	@Returns:
+	The IDI of the paper. Max and min IDI corresponding to the number of citations are also returned.
+	"""
 	IDT = get_idt_till_year(IDT, year, PAPER_YEAR_DICT)
 
 	if len(IDT.edges()) == 0:
@@ -390,32 +394,32 @@ The IDI of the paper. Max and min IDI corresponding to the number of citations a
 
 	leaf_nodes = []
 	for v in nodes:
-        in_edges = IDT.in_edges(nbunch = [v])
-        if len(in_edges) == 0:
-            leaf_nodes.append(v)
+		in_edges = IDT.in_edges(nbunch = [v])
+		if len(in_edges) == 0:
+			leaf_nodes.append(v)
 
-    v_count = {i : 0 for i in nodes}
-    for v in leaf_nodes:
-        paths = list(nx.all_shortest_paths(IDT , source = v , target = cur_paper))
-        for path in paths:
-            for e in path:
-                v_count[e] += 1
+	v_count = {i : 0 for i in nodes}
+	for v in leaf_nodes:
+		paths = list(nx.all_shortest_paths(IDT , source = v , target = cur_paper))
+		for path in paths:
+			for e in path:
+				v_count[e] += 1
 
-    idi = 0
-    for i in nodes:
-    	idi += v_count[i]
+	idi = 0
+	for i in nodes:
+		idi += v_count[i]
 
 	return idi, get_max_idi(num_citations), get_min_idi(num_citations)
 
 def get_idt_till_year(IDT, year, PAPER_YEAR_DICT):
-"""
-@Params: 
-IDT: IDT of the paper 
-year: Year untill which IDI is to be calculated. Nodes will be removed from the IDT accordingly.
-PAPER_YEAR_DICT: Dictionary with paper ids as keys and year of publication as values.
-@Returns:
-The pruned IDT of the paper. Papers (nodes) published after 'year' are removed from the IDT.
-"""
+	"""
+	@Params: 
+	IDT: IDT of the paper 
+	year: Year untill which IDI is to be calculated. Nodes will be removed from the IDT accordingly.
+	PAPER_YEAR_DICT: Dictionary with paper ids as keys and year of publication as values.
+	@Returns:
+	The pruned IDT of the paper. Papers (nodes) published after 'year' are removed from the IDT.
+	"""
 	nodes = list(IDT.nodes())
 	cur_paper = None
 	for v in nodes:
@@ -429,21 +433,21 @@ The pruned IDT of the paper. Papers (nodes) published after 'year' are removed f
 		exit(1)
 
 	induced_graph = set()
-    induced_graph_list = list(IDT.in_edges(nbunch = [cur_paper]))
+	induced_graph_list = list(IDT.in_edges(nbunch = [cur_paper]))
 
-    for e in induced_graph_list:
-        if e[0] in PAPER_YEAR_DICT and int(PAPER_YEAR_DICT[e[0]]) <= year:
-            induced_graph.add(e[0])
-    
-    induced_graph.add(cur_paper)
-    H = IDT.subgraph(induced_graph)
-    H = nx.DiGraph(H)
-    
-    shortest_path = {}
-    for e in H.nodes():
-        e_list = list(H.out_edges(nbunch = [e]))
-        if len(e_list) > 1:
-            H.remove_edge(e , cur_paper)
+	for e in induced_graph_list:
+		if e[0] in PAPER_YEAR_DICT and int(PAPER_YEAR_DICT[e[0]]) <= year:
+			induced_graph.add(e[0])
+	
+	induced_graph.add(cur_paper)
+	H = IDT.subgraph(induced_graph)
+	H = nx.DiGraph(H)
+	
+	shortest_path = {}
+	for e in H.nodes():
+		e_list = list(H.out_edges(nbunch = [e]))
+		if len(e_list) > 1:
+			H.remove_edge(e , cur_paper)
 
-    return H
+	return H
 
